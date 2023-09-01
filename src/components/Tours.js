eb0725733f8fd4e00380e309e54977b0565d9278
abhi-yo <akshatsing11@gmail.com>
2023-09-01 13:47:59 +0530

    add files

---
+++ src/components/Tours.js
@@ -0,0 +1,16 @@
+import Card from "./Card";
+function Tours({ tours, removeTour }) {
+  return (
+    <div className="container">
+      <div>
+        <h2 className="title">Plan with Me</h2>
+      </div>
+      <div className="cards">
+        {tours.map((tour) => {
+          return <Card {...tour} removeTour={removeTour}></Card>;
+        })}
+      </div>
+    </div>
+  );
+}
+export default Tours;
